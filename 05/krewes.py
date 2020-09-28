a8221b6a32bc1db2428a0f0a93ebec54b8c2b3e7
Pak Ming Lau <plau10@stuy.edu>
2020-09-27 20:17:15 -0400

    Update krewes.py to match with team members

--- 05/krewes.py
+++ 05/krewes.py
@@ -1,3 +1,9 @@
+# Pak Ming Lau
+# SoftDev
+# K05 -- Teamwork, but Better This Time
+#   Write a program that prints the name of a randomly selected student from KREWES
+# 2020-09-30
+
 import random
 
 KREWES = {
@@ -6,33 +12,6 @@ KREWES = {
     'endymion': ['JASON', 'DEAN', 'MADDIE', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
 }
 
-def printRandomName():
-    team = random.choice(tuple(KREWES.keys()))
-    print("\n" + random.choice(KREWES[team]) + " of team " + team + "\n")
-
-def printRandomNameOfChosenTeam():
-    team = input("Please type in the name of the team you want to randomly select a person from.(orpheus, rex, or endymion)\n").lower()
-    isTeamName = False
-    while not isTeamName:
-        if(team != "orpheus" and team != "rex" and team != "endymion"):
-            team = input("Error, not a team name, please input a valid team name\n").lower()
-        else:
-            isTeamName = True
-    print("\n" + random.choice(KREWES[team]) + " of team " + team + "\n")
-
-def main():
-    isAnswerPossible = False
-    answer = None
-    while not isAnswerPossible:
-        answer = input("What would you like to do? (Input the number of your choice)\n0. Receive a random name from KREWES\n1. Choose a team to receive a random name from\n2. Exit\n")
-        if(answer != "0" and answer != "1" and answer != "2"):
-            print("Error, not a valid choice")
-        elif(answer == "0"):
-            printRandomName()
-        elif(answer == "1"):
-            printRandomNameOfChosenTeam()
-        else:
-            break
-
-
-main()
+randTeam = random.choice(list(KREWES.keys()))
+team = KREWES[randTeam]
+print(random.choice(team))
