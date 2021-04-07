57fcdac90fe8da25d81e5f0fbdb17404bccdcbfe
Pak Ming Lau <pak.plau@gmail.com>
2021-04-07 10:04:49 -0400

    Add testing files for restful apis

---
+++ restful_api/nyt_movies_app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import urllib, json
+
+app = Flask(__name__)
+
+f = open("key_nasa.txt", "r")
+link = "https://api.nasa.gov/planetary/apod?api_key=" + f.read()
+f.close()
+
+@app.route("/", methods=["GET"])
+def root():
+    u = urllib.request.urlopen(link)
+    data = json.loads(u.read())
+    return render_template("main.html", image=data["url"], explanation=data["explanation"])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
