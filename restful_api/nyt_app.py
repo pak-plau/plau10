9b89ed8ef3a471c7ab33f696ab996281e39991b6
Pak Ming Lau <pak.plau@gmail.com>
2021-04-25 20:52:33 -0400

    Add files to test for nyt movie reveiws

---
+++ restful_api/nyt_app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import urllib, json
+
+app = Flask(__name__)
+
+f = open("keys/nytimes.txt", "r")
+nyt_link = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=john%20wick&api-key=" + f.read()
+f.close()
+
+f = open("keys/tmdb.txt", "r")
+tmdb_link = "https://api.themoviedb.org/3/search/movie?query=john%20wick&api_key=" + f.read()
+f.close()
+
+@app.route("/", methods=["GET"])
+def root():
+    u = urllib.request.urlopen(tmdb_link)
+    data1 = json.loads(u.read())
+    u = urllib.request.urlopen(nyt_link)
+    data = json.loads(u.read())
+    return render_template("data.html", data=data, extra=data["results"][0]["display_title"], data1 = data1, extra1=data1["results"][0]["original_title"])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
